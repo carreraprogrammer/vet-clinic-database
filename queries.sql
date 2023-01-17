94f6393130f27e9bdfc9b145850af454cc5a1b22
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-17 10:50:31 -0500

    Add query for animals between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -2,3 +2,8 @@
 
     SELECT * FROM animals
     WHERE name LIKE '%mon';
+
+/* List the name of all animals born between 2016 and 2019. */
+
+    SELECT * FROM animals
+    WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';

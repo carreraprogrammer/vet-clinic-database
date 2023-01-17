70f3d2e574f39102c00736a56514c6f5b80ec802
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-17 10:56:33 -0500

    Add query for neutered and less than 3 escape attempts

--- queries.sql
+++ queries.sql
@@ -7,3 +7,8 @@
 
     SELECT * FROM animals
     WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+/* List the name of all animals that are neutered and have less than 3 escape attempts. */
+
+    SELECT * FROM animals
+    WHERE neutered = true AND escape_attempts < 3;

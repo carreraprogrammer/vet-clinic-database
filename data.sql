e00fe02e46f8a4480f1c3f2036aaa37da2a0bc12
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-17 10:42:00 -0500

    Insert data into animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
+SELECT 'Agumon','2020-02-03',0, true,10.23
+UNION
+SELECT 'Gabumon','2018-11-15',2, true,8
+UNION
+SELECT 'Pikachu','2021-01-07',1, false,15.04
+UNION
+SELECT 'Devimon','2017-05-12',5, true,11;

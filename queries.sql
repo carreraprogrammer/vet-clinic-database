9d34bbf68d939863db2700a34ffdf9e96710786b
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-17 11:04:33 -0500

    Add query for escape atemnts of animals less than 10.5kg

--- queries.sql
+++ queries.sql
@@ -17,3 +17,8 @@
 
     SELECT date_of_birth FROM animals
     WHERE name = 'Agumon' OR name = 'Pikachu';
+
+/* List name and escape attempts of animals that weigh more than 10.5kg */
+
+    SELECT name, escape_attempts FROM animals
+    WHERE weight_kg > 10.5;

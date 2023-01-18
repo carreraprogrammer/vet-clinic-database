d8240e401ea06f822042f84a8297a25be1844ba9
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-18 18:12:29 -0500

    Change weights to positive numbers

--- queries.sql
+++ queries.sql
@@ -37,3 +37,63 @@
 
     SELECT * FROM animals
     WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/* Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction. */
+
+BEGIN;
+
+UPDATE animals SET species='unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+/* Change the species from animals ended in mon */
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+SELECT * FROM animals
+
+COMMIT;
+
+SELECT * FROM animals
+
+/* Inside a transaction delete all records in the animal's table, then roll back the transaction. */
+
+BEGIN;
+
+DELETE FROM animals;
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+/* Delete all animals born after Jan 1st, 2022. */
+
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SP1
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg  < 0;
+
+COMMIT;

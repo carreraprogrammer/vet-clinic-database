fe7979494c1945a176361db0d65d31f6fad6be15
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-18 10:58:40 -0500

    Add column species to animals table

--- data.sql
+++ data.sql
@@ -1,10 +1,17 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-SELECT 'Agumon','2020-02-03',0, true,10.23
+SELECT 'Agumon', '2020-02-03', 0, true, 10.23
 UNION
-SELECT 'Gabumon','2018-11-15',2, true,8
+SELECT 'Gabumon', '2018-11-15', 2, true, 8
 UNION
-SELECT 'Pikachu','2021-01-07',1, false,15.04
+SELECT 'Pikachu', '2021-01-07', 1, false, 15.04
 UNION
-SELECT 'Devimon','2017-05-12',5, true,11;
+SELECT 'Devimon', '2017-05-12', 5, true, 11;
+
+/* Add a column species of type string to your animals table. Modify your schema.sql file. */
+
+BEGIN;
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(200);
+COMMIT;

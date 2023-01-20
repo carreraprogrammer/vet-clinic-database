95195c024da58050eb55e5eab9c0cdfdc4e2db2e
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-19 19:13:57 -0500

    Add query for digimons owned by Jennifer Orwell

--- queries.sql
+++ queries.sql
@@ -164,3 +164,13 @@ FROM animals a
 JOIN species s
 ON a.species_id = s.id
 GROUP BY s.name;
+
+/* List all Digimon owned by Jennifer Orwell. */
+
+SELECT a.name, o.full_name, s.name
+FROM animals a
+JOIN species s
+ON a.species_id = s.id
+JOIN owners o
+ON a.owner_id = o.id
+WHERE o.full_name = 'Jennifer Orwell' AND s.name ='Digimon';

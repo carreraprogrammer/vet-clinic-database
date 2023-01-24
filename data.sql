3b90eaf132f342724c6b8e578b65c94dbf211927
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-23 20:20:57 -0500

    Initial commit

--- data.sql
+++ data.sql
@@ -112,3 +112,6 @@ VALUES
     (9, 2, '2020-08-03'),
     (10, 3, '2020-05-24'),
     (10, 1, '2021-01-11');
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
